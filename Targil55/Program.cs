e0bb3361fae43e6c7e259753f2eaacdc928b83b3
Brami <EYBR@ferring.com>
2020-01-26 21:04:17 +0200

    Add project files.

---
+++ Targil55/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+/*
+  כיתבו תוכנית המדמה פעולה של מכונה למכירת כרטיסים לקולנוע. בתחילה התוכנית (המכונה) מודיעה למשתמש מה המחיר של כרטיס (יוגדר כקבוע וערכו 50 שקלים) ואז שואלת אותו כמה כרטיסים ברצונו לקנות. לאחר שהיא מקבלת את תשובתו, היא כותבת לו מה המחיר הכולל שהוא צריך לשלם. היא "מקבלת" את כספו (קוראת מהקלט כמה הכניס). אם הסכום מדוייק - היא מדפיסה הודעה על מספר הכרטיסים שמחכים לו בקופה. אם חסר כסף, היא מודיעה למשתמש כמה כסף חסר. אם המשתמש הכניס יותר מידי כסף, המכונה מדפיסה הודעה על מספר הכרטיסים המחכים לו בקופה, וגם מודיעה לו על העודף המגיע לו. חובה לבדוק תקינות הקלט. את הקוד כיתבו במחלקה שתקרא TicketMachine. על הפונקציה להיות סטטית.
+
+     */
+namespace Targil55
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numOfTickets;
+            TicketMachine ticketMachine = new TicketMachine();
+            Console.WriteLine("Welcome - Ticket Price " + ticketMachine.GetPrice() + " $");
+            Console.WriteLine("Hello, please choose number of tickets ");
+            numOfTickets = int.Parse(Console.ReadLine());
+            Console.WriteLine("Hello, please choose number of tickets " + ticketMachine.GetTotal(numOfTickets));
+            Console.ReadLine();
+        }
+
+    }
+}
---
+++ Targil55/TicketMachine.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Targil55
+{
+    public class TicketMachine
+    {
+        private int Price;
+        public int GetPrice()
+        {
+            this.Price = 50;
+            return Price;
+        }
+        public int GetTotal(int num)
+        {
+            if (num < 1)
+                return 0;// Console.WriteLine("Please choose quantity more than 0 !");
+            return num * GetPrice();
+        }
+    }
+}
